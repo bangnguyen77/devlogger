06e286b7851b77ad19e9ec7b0d980790bdcfbf66
Bang Nguyen <bangnv77@gmail.com>
2016-12-06 12:47:18 -0500

    create logs component

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {LogsComponent} from './components/logs/logs.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, LogsComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
